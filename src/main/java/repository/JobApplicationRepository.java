106e49175e738d30556e5deb0cebf8cd629e9f0f
Jordan Bannister <40512499+Jordanb1997@users.noreply.github.com>
2023-09-17 11:17:40 +1000

    Created simple jobApplication repository

---
+++ src/main/java/repository/JobApplicationRepository.java
@@ -0,0 +1,30 @@
+package repository;
+
+import model.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+
+    Optional<JobApplication> findById(Long id);
+
+    List<JobApplication> findByCompanyName(String name);
+
+    List<JobApplication> findByJobTitle(String name);
+
+    List<JobApplication> findByExperienceLevel(String name);
+
+    List<JobApplication> findByEmploymentType(String name);
+
+    List<JobApplication> findByOnsitePolicy(String name);
+
+    List<JobApplication> findByApplicationStatus(String name);
+
+    //date range
+
+    //response date
+
+    //complex queries??
+}
